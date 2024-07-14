03221af882228511c2cfb23fc46cb7d2daa968ef
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-14 20:24:58 +0530

    sumOffOddNumbers

--- findLargest_number.js
+++ findLargest_number.js
@@ -1,11 +1,12 @@
 const Student_marks= [99,40,50,67,80,65,70,55,88]  //Student Marks Array
-let temp_large = Student_marks[0]; 
-for(const num of Student_marks) //for loop
-{
-    if(num>temp_large)
-    {
-        const largest_num = num;
-    
-    }
+let largest = Student_marks[0]; 
+
+
+for (var i = 0; i < Student_marks.length; i++) {
+  if (Student_marks > largest ) {
+    largest = Student_marks [i];
+  }
 }
-console.log(largest_num) // printing largest element 99
+console.log(`largest_num ${ largest}`);
+
+ // printing largest element 99
