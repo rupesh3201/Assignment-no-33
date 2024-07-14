1e7ef300af9400ba395570a6ad91d1d5d4401712
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-14 20:03:17 +0530

    temp variable decelare

--- findLargest_number.js
+++ findLargest_number.js
@@ -1,1 +1,11 @@
 const Student_marks= [99,40,50,67,80,65,70,55,88]  //Student Marks Array
+let temp_large = Student_marks[0]; 
+for(const num of Student_marks) //for loop
+{
+    if(num>temp_large)
+        {
+            const largest_num = num;
+    
+        }
+}
+console.log(largest_num)
