77d3cb8d278a186b9a0d018092c04f90a91b778d
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-14 20:28:07 +0530

    for condtion

--- sumofodd_Number.js
+++ sumofodd_Number.js
@@ -1,3 +1,8 @@
 function sumOffOddNumbers(arr) {
     let sum = 0;
-    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            sum += arr[i];
+        }
+    }
+ 
